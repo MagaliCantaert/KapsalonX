7cd00088c1f3ce403b29bf9fb6dbff50f6b2edba
Axelle Hillewaere <axelle.hillewaere@hotmail.be>
2018-10-28 18:04:53 +0100

    Add start of project

---
+++ EE.KapsalonX/EE.KapsalonX/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace EE.KapsalonX.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
