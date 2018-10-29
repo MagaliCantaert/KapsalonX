793b6db6ba96858084004c7055ed252cfc75e875
Axelle Hillewaere <axelle.hillewaere@hotmail.be>
2018-10-29 17:19:21 +0100

    Toevoegen van BoekenController en start van weergave op view

---
+++ EE.KapsalonX/EE.KapsalonX/Controllers/BoekenController.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using EE.KapsalonX.Web.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EE.KapsalonX.Web.Controllers
+{
+    public class BoekenController : Controller
+    {
+        List<BehandelingVm> BehandelingenDames = new List<BehandelingVm>
+        {
+            new BehandelingVm { Coupe = "Kort haar", Optie = "Knippen", Tijdsduur = new TimeSpan(00,30,00)},
+            new BehandelingVm { Coupe = "Kort haar", Optie = "Kleuren", Tijdsduur = new TimeSpan(00,45,00)},
+            new BehandelingVm { Coupe = "Kort haar", Optie = "Brushing", Tijdsduur = new TimeSpan(00,30,00)},
+            new BehandelingVm { Coupe = "Kort haar", Optie = "Knippen + kleuren", Tijdsduur = new TimeSpan(01,15,00)},
+            new BehandelingVm { Coupe = "Kort haar", Optie = "Knippen + kleuren + brushing", Tijdsduur = new TimeSpan(01,45,00)},
+
+            new BehandelingVm { Coupe = "Lang haar", Optie = "Knippen", Tijdsduur = new TimeSpan(00,40,00)},
+            new BehandelingVm { Coupe = "Lang haar", Optie = "Kleuren", Tijdsduur = new TimeSpan(01,00,00)},
+            new BehandelingVm { Coupe = "Lang haar", Optie = "Brushing", Tijdsduur = new TimeSpan(00,40,00)},
+            new BehandelingVm { Coupe = "Lang haar", Optie = "Knippen + kleuren", Tijdsduur = new TimeSpan(01,40,00)},
+            new BehandelingVm { Coupe = "Lang haar", Optie = "Knippen + kleuren + brushing", Tijdsduur = new TimeSpan(02,20,00)}
+        };
+
+        List<BehandelingVm> BehandelingenHeren = new List<BehandelingVm>
+        {
+            new BehandelingVm { Optie = "Snit", Tijdsduur = new TimeSpan(00,30,00) },
+            new BehandelingVm { Optie = "Tondeuse", Tijdsduur = new TimeSpan(00,30,00) },
+            new BehandelingVm { Optie = "Knippen + kleuren", Tijdsduur = new TimeSpan(01,00,00)}
+        };
+
+        List<BehandelingVm> BehandelingenKinderen = new List<BehandelingVm>
+        {
+            new BehandelingVm { Optie = "Snit meisjes", Tijdsduur = new TimeSpan(00,30,00)},
+            new BehandelingVm { Optie = "Snit jongens", Tijdsduur = new TimeSpan(00,30,00)}
+        };
+
+        public IActionResult Index()
+        {
+            var viewModel = new BoekenIndexVm();
+            viewModel.BehandelingenDames = BehandelingenDames;
+            viewModel.BehandelingenHeren = BehandelingenHeren;
+            viewModel.BehandelingenKinderen = BehandelingenKinderen;
+            return View("Index", viewModel);
+        }
+
+        public IActionResult DamesBehandeling()
+        {
+            var viewModel = new BoekenIndexVm();
+            viewModel.BehandelingenDames = BehandelingenDames;
+            ViewBag.Behandeling = BehandelingenDames;
+            return View("Index", viewModel);
+        }
+
+    }
+}
---
+++ EE.KapsalonX/EE.KapsalonX/Models/BoekenIndexVm.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EE.KapsalonX.Web.Models
+{
+    public class BoekenIndexVm
+    {
+        public Guid KlantId { get; set; }
+        public string Voornaam { get; set; }
+        public string Achternaam { get; set; }
+        public string Emailadres { get; set; }
+        public string Telefoonnummer { get; set; }
+
+        public List<BehandelingVm> BehandelingenDames { get; set; }
+        public List<BehandelingVm> BehandelingenHeren{ get; set; }
+        public List<BehandelingVm> BehandelingenKinderen { get; set; }
+
+    }
+}
