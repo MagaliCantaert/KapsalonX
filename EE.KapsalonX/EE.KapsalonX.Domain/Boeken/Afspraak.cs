ad2419be0b2df4d50cb2966ad1cfd36a79015ef3
Axelle Hillewaere <axelle.hillewaere@hotmail.be>
2018-10-29 14:04:58 +0100

    Toevoegen van de klassen voor de boekingstool

---
+++ EE.KapsalonX/EE.KapsalonX.Domain/Boeken/Afspraak.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EE.KapsalonX.Domain.Boeken
+{
+    public class Afspraak
+    {
+        public Guid AfspraakId { get; set; }
+        public Klant KlantGegevens { get; set; }
+        public Behandeling BehandelingGegevens { get; set; }
+        public DateTime DatumTijdstip { get; set; }
+
+    }
+}
---
+++ EE.KapsalonX/EE.KapsalonX.Domain/Boeken/Klant.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EE.KapsalonX.Domain.Boeken
+{
+    public class Klant
+    {
+        public Guid KlantId { get; set; }
+        public string Voornaam { get; set; }
+        public string Achternaam { get; set; }
+        public string Emailadres { get; set; }
+        public string Telefoonnummer { get; set; }
+        public ICollection<Afspraak> Afspraken
+    }
+}
