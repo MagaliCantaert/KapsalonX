58ce287c23c1b75b62bca43b3068e0a607266566
Axelle Hillewaere <axelle.hillewaere@hotmail.be>
2018-11-17 17:00:12 +0100

    Toevoegen van ContactController

---
+++ EE.KapsalonX/EE.KapsalonX/Controllers/ContactController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using EE.KapsalonX.Web.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EE.KapsalonX.Web.Controllers
+{
+    public class ContactController : Controller
+    {
+        public IActionResult Index()
+        {
+            var viewModel = new ContactIndexVm();
+            return View(viewModel);
+        }
+
+        [HttpPost]
+        [AutoValidateAntiforgeryToken]
+        public IActionResult Index(ContactIndexVm viewModel)
+        {
+            if (ModelState.IsValid)
+            {
+                return new RedirectToActionResult("Index", "Contact", null);
+            }
+            else
+            {
+                return View(viewModel);
+            }
+        }
+    }
+}
